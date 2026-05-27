69597fa9f3ddf9b25df335774f092587b0e430b0
Gufucuvf <119861845+Gufucuvf@users.noreply.github.com>
2026-05-27 21:10:33 +0530

    Fix Streamlit deployment

--- ai-powered-insider-threat-detection-system-main/ai-powered-insider-threat-detection-system-main/explainability/explain.py
+++ ai-powered-insider-threat-detection-system-main/ai-powered-insider-threat-detection-system-main/explainability/explain.py
@@ -22,16 +22,18 @@ print(f'Explaining anomaly for user: {top_user}')
 # Load model
 iso = joblib.load(os.path.join(MODEL_DIR, 'isolation_forest.pkl'))
 
-# LIME explanation
+# LIME 
 explainer = LimeTabularExplainer(X, feature_names=features.columns[1:], discretize_continuous=True)
 exp = explainer.explain_instance(X[user_idx], iso.decision_function)
 print('\nLIME Explanation:')
 for f, w in exp.as_list():
     print(f'{f}: {w:.3f}')
 
-# SHAP explanation
+# SHAP 
 explainer_shap = shap.Explainer(iso, X)
 shap_values = explainer_shap(X[user_idx:user_idx+1])
 print('\nSHAP Explanation:')
 for name, value in zip(features.columns[1:], shap_values.values[0]):
     print(f'{name}: {value:.3f}') 
+    
+    
